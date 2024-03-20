23343ce017b156b1b9d0bb71597a58c05251c37e
Stckrz <rin449@gmail.com>
2024-03-19 20:47:06 -0500

    made a new route for getting an item by string match

--- routes/sale-item-routes.js
+++ routes/sale-item-routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router()
 const SaleItemModel = require('../models/sale-item');
-const { isLoggedIn } = require('../middleware/middleware'); 
+const { isLoggedIn } = require('../middleware/middleware');
 
 //get all sale-items
 router.get('/getAll/', async (req, res) => {
-	try{
+	try {
 		const data = await SaleItemModel.find();
 		res.json(data)
 	}
@@ -17,8 +17,19 @@ router.get('/getAll/', async (req, res) => {
 //get all sale-items of a specific category
 router.get('/getAllByCategory/:category', async (req, res) => {
 	const category = req.params.category;
-	try{
-		const data = await SaleItemModel.find( { "category": category } )
+	try {
+		const data = await SaleItemModel.find({ "category": category })
+		res.json(data)
+	}
+	catch (error) {
+		res.status(400).json({ message: error.message })
+	}
+})
+
+router.get('/getOneByString/:string', async (req, res) => {
+	const string = req.params.string
+	try {
+		const data = await SaleItemModel.find({ "name": {$regex: string, $options: "i"} })
 		res.json(data)
 	}
 	catch (error) {
@@ -29,7 +40,7 @@ router.get('/getAllByCategory/:category', async (req, res) => {
 //get one sale-item by id
 router.get('/getOne/:id', async (req, res) => {
 	const id = req.params.id
-	try{
+	try {
 		const data = await SaleItemModel.findById(id)
 		res.json(data)
 	}
@@ -41,7 +52,7 @@ router.get('/getOne/:id', async (req, res) => {
 //delete one sale-item by id
 router.delete('/delete/:id', async (req, res) => {
 	const id = req.params.id
-	try{
+	try {
 		const data = await SaleItemModel.findByIdAndDelete(id)
 		res.json(data)
 	}
